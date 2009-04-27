7f18a83f91bc5e1af639ff8556e1632ef117bf6b
Dan Blanchard <dan.blanchard@gmail.com>
2009-04-27 22:16:08 +0000

    - Modified CMU Dictionary to include all words in BR corpus. - Added orthographic version of BR corpus to repository. - Created addStress perl script to convert an orthographic corpus to a CMU-style one with primary stress.

---
+++ code/addStress.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+# Dan Blanchard
+# Looks up stress info in CMU-dict and adds it back into corpus
+
+use strict;
+use POSIX;
+use utf8;
+use Encode;
+
+my %cmuDict;
+my @dictPair;
+my @line;
+my $stressedWord;
+
+# Setup Unicode input and output
+binmode STDOUT, ":utf8";
+binmode STDIN, ":utf8";
+binmode STDERR, ":utf8";
+
+my $usage = "\nUsage: ./dict-helper.pl CMU-DICT-FILE [ORTHOGRAPHIC-CORPORA]\n\n";
+die $usage if @ARGV < 1;
+my $dictFile = shift @ARGV;
+
+open(DICTFILE, $dictFile);
+binmode DICTFILE, ":utf8";
+while (<DICTFILE>)
+{
+	chomp;
+	if (($_ !~ m/[(){};"#%&!]/) && ($_ !~ m/^['.\-,\/?:]/)) # Ignore lines that start with junk (or are alternative pronunciations)
+	{
+		@dictPair = split /  /,$_;
+		$cmuDict{$dictPair[0]} = $dictPair[1];
+	}	
+}
+close(DICTFILE);
+
+while (<>)
+{
+	chomp;
+	@line = split / /,$_;
+	foreach my $word (@line)
+	{
+		$word = uc($word);
+		if (exists $cmuDict{$word})
+		{
+			$stressedWord = $cmuDict{$word};
+		}
+		elsif ($word =~ m/'S$/)
+		{
+			$word =~ s/'S$/S/;
+			if (exists $cmuDict{$word})
+			{
+				$stressedWord = $cmuDict{$word};
+			}
+			else
+			{
+				die "Word not found: $word\n";
+			}
+		}
+		else
+		{
+			die "Word not found: $word\n";
+		}
+		$stressedWord =~ s/[02]//g;
+		print "$stressedWord#"
+	}
+	print "\n";
+}
