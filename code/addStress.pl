f2610c096020a295833254bfe80e8c3e93815f3a
Dan Blanchard <dan.blanchard@gmail.com>
2009-04-29 20:33:06 +0000

    Changed how addStress works, although I now believe the stress angle is unrealistic.

--- code/addStress.pl
+++ code/addStress.pl
@@ -10,17 +10,25 @@ use Encode;
 
 my %cmuDict;
 my @dictPair;
-my @line;
+my @phonWords;
+my @orthWords;
+my @cmuPhones;
+my $phonLine;
+my $orthLine;
+my $phonWord;
+my $orthWord;
 my $stressedWord;
+my $wordDelimiter = " ";
 
 # Setup Unicode input and output
 binmode STDOUT, ":utf8";
 binmode STDIN, ":utf8";
 binmode STDERR, ":utf8";
 
-my $usage = "\nUsage: ./dict-helper.pl CMU-DICT-FILE [ORTHOGRAPHIC-CORPORA]\n\n";
-die $usage if @ARGV < 1;
+my $usage = "\nUsage: ./dict-helper.pl CMU-DICT-FILE ORTHOGRAPHIC-CORPUS [PHONETIC-CORPUS]\n\n";
+die $usage if @ARGV < 2;
 my $dictFile = shift @ARGV;
+my $orthFile = shift @ARGV;
 
 open(DICTFILE, $dictFile);
 binmode DICTFILE, ":utf8";
@@ -35,32 +43,52 @@ while (<DICTFILE>)
 }
 close(DICTFILE);
 
-while (<>)
+open(ORTHFILE, $orthFile);
+binmode ORTHFILE, ":utf8";
+while ($phonLine = <>)
 {
-	chomp;
-	@line = split / /,$_;
-	foreach my $word (@line)
+	chomp $phonLine;
+	$orthLine = <ORTHFILE>;
+	chomp $orthLine;
+	@phonWords = split /\Q$wordDelimiter\E/,$phonLine;
+	@orthWords = split /\Q$wordDelimiter\E/,$orthLine;
+	for (my $i = 0; $i < scalar(@orthWords); $i++) 	
 	{
 		$stressedWord = "";
-		$word = uc($word);
-		if (exists $cmuDict{$word})
+		$orthWord = uc($orthWords[$i]);
+		$phonWord = $phonWords[$i];
+		if (exists $cmuDict{$orthWord})
 		{
-			$stressedWord = $cmuDict{$word};
+			$stressedWord = $cmuDict{$orthWord};
 		}
 		else
 		{
-			$word =~ s/'S$/S/;
-			$stressedWord = $cmuDict{$word};
+			$orthWord =~ s/'S$/S/;
+			$stressedWord = $cmuDict{$orthWord};
 		}
 		if (!$stressedWord)
 		{
-			die "Word not found: $word\n";
+			die "Word not found: $orthWord\n";
 		}
 		else
 		{
 			$stressedWord =~ s/[02]//g; # Remove unstressed and secondary-stress symbols
-			print "$stressedWord#"			
+			@cmuPhones = split / /,$stressedWord;
+			if (length($phonWord) == scalar(@cmuPhones))
+			{
+				for (my $j = 0; $j < scalar(@cmuPhones); $j++) 
+				{
+					if ($cmuPhones[$j] =~ m/1/)
+					{
+						substr($phonWord, $j+1, 0) = '\'';
+						last; # Can stop, since words have at most one primary stress
+					}
+				}
+				
+			}
+			print "$phonWord$wordDelimiter";
 		}
 	}
 	print "\n";
 }
+close(ORTHFILE);
