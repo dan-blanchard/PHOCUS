448976e30c8731a3a1d9680d93eca1fe3fb6213a
Dan Blanchard <dan.blanchard@gmail.com>
2009-11-23 01:15:34 +0000

    Cross-validation tool:  Can now specify number of training sets.

--- code/crossvalidate.pl
+++ code/crossvalidate.pl
@@ -23,7 +23,7 @@ use Getopt::Std;
 use POSIX;
 
 local $| = 1;
-our ($opt_k,$opt_p,$opt_e,$opt_i,$opt_v,$opt_d);
+our ($opt_k,$opt_p,$opt_e,$opt_t,$opt_v,$opt_d);
 my $usage = "\nK-Fold Cross-Validater\n\n" . 
 			"Usage: ./crossvalidate.pl [OPTIONS] SEGMENTATION_COMMAND TRUE_CORPUS\n\n" . 
 			"SEGMENTATION_COMMAND = A string (enclosed by quotes) that specifies the segmenter to run" .
@@ -34,27 +34,34 @@ my $usage = "\nK-Fold Cross-Validater\n\n" .
 			"\t-e FLAGS\tFlags to pass on to errors.pl. (Must enclose in quotes.)\n" .
 			"\t-k NUM_FOLDS\tSpecifies number of folds for cross-validation. (Default = 10)\n" .
 			"\t-p PREFIX\tFile prefix for temporary fold files. Note: All files with this prefix are DELETED when script ends. (Default = 'cvfoldtemp')\n" .
+			"\t-t TRAINING_FOLDS\tNumber of folds to train on.  (Default = 1)\n" .
 			"\t-d \t\tDo NOT delete files that begin with PREFIX when done. (Although any existing ones are still destroyed when the script is first run.)\n" .
-			"\t-i \t\tInvert training and test sets.  With 10 folds, that would mean train on 1 and test on 9.\n" .
 			"\t-v \t\tOutput result of running errors.pl for each individual fold.\n";
 
 # Handle arguments
-getopts('e:k:p:div');
+getopts('e:k:p:t:dv');
 die $usage if @ARGV < 2;
 my $segmenter = shift @ARGV;
 my $corpus = shift @ARGV;
 
 my $prefix = "cvfoldtemp";
 my $folds = 10;
+my $trainingFolds = 1;
 
 if ($opt_p)
 {
 	$prefix = $opt_p;
 }
+$prefix = $prefix . time();
 
 if ($opt_k)
 {
 	$folds = $opt_k;
+}
+
+if ($opt_t)
+{
+	$trainingFolds = $opt_t;
 }
 
 my @allFiles = <$prefix*>;
@@ -66,55 +73,44 @@ my $corpusLength = `wc -l $corpus | perl -p -e 's/^[ \\t]+([0-9]+)[ \\t].*\$/\$1
 my $foldSize = floor($corpusLength / $folds);
 my $suffixLength = ceil(log($folds) / log(26));
 `split -a $suffixLength -l $foldSize $corpus $prefix`;
-my @selection = ();
-my %fold;
+my %training;
+my @test;
 my $ir;
 @allFiles = <$prefix*>;
-my $trainingSize = $corpusLength - $foldSize;
+my $trainingSize = $foldSize * $trainingFolds;
+my $testSize = $corpusLength - $trainingSize;
+my @currentFiles;
+my $trainingIndex;
 for (my $i = 0; $i < $folds; $i++)
 {
-	my @currentFiles = @allFiles;
-	@selection = ();
+	@currentFiles = @allFiles;
+	%training = ();
+	@test = ();
 	if ($i > 0)
 	{
 		`rm $prefix-gold-current`;		
 	}
-	if ($opt_i)
+	for (my $j = 0; $j < $trainingFolds; $j++)
 	{
-		`cat $allFiles[$i] >> $prefix-gold-current`;
-		splice(@currentFiles, $i, 1);
-		for (my $j = 0; $j < $folds - 1; $j++)
-		{
-			$ir = rand @currentFiles;
-			push @selection, $currentFiles[$ir];
-			splice(@currentFiles, $ir, 1);
-		}
-		foreach my $f (@selection)
-		{
-			`cat $f >> $prefix-gold-current`;
-		}
-		print "Segmenting fold $i...";
-		`$segmenter $foldSize $prefix-gold-current | tail -n $trainingSize >> $prefix-results`;
-		`tail -n $trainingSize $prefix-gold-current >> $prefix-gold`;
+		$trainingIndex = ($j + $i) % $folds;
+		$training{$trainingIndex} = 1;
+		`cat $allFiles[$trainingIndex] >> $prefix-gold-current`;
 	}
-	else
+	for (my $j = 0; $j < $folds; $j++)
+	{			
+		if (!$training{$j})
+		{
+			push @test, $currentFiles[$j];
+			# Should probably add randomization code back in here.
+		}
+	}
+	foreach my $f (@test)
 	{
-		splice(@currentFiles, $i, 1);
-		for (my $j = 0; $j < $folds - 1; $j++)
-		{
-			$ir = rand @currentFiles;
-			push @selection, $currentFiles[$ir];
-			splice(@currentFiles, $ir, 1);
-		}
-		foreach my $f (@selection)
-		{
-			`cat $f >> $prefix-gold-current`;
-		}
-		`cat $allFiles[$i] >> $prefix-gold-current`;		
-		print "Segmenting fold $i...";
-		`$segmenter $trainingSize $prefix-gold-current | tail -n $foldSize >> $prefix-results`;
-		`tail -n $foldSize $prefix-gold-current >> $prefix-gold`;
+		`cat $f >> $prefix-gold-current`;
 	}
+	print "Segmenting fold $i...";
+	`$segmenter $trainingSize $prefix-gold-current | tail -n $testSize >> $prefix-results`;
+	`tail -n $testSize $prefix-gold-current >> $prefix-gold`;
 	print "done\n";
 	if ($opt_v)
 	{
