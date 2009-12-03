6a77bc89706602190b81f10061d0374841c7b2b3
Dan Blanchard <dan.blanchard@gmail.com>
2009-12-03 05:19:38 +0000

    Made syllabify.pl not infinitely loop when there are no syllabic sounds on a line.

--- code/syllabify.pl
+++ code/syllabify.pl
@@ -34,36 +34,38 @@ while ($line = <>)
 	@breaks = ();
 	chomp $line;
 	$syllables = $line;
-	$syllables =~ s/(9I)|(9U)|(OI)/--/g;  # diphthongs
-	$syllables =~ s/[&69AEIOUaeiouR~ML]/length($`)%10/eg; # vowels
-	$syllables =~ s/--/(length($`)%10) x 2/eg;  # diphthongs
-	# Loop until we've assigned all characters to a syllable
-	while ($syllables =~ m/[^0-9]/)
+	if ($syllables =~ m/[&69AEIOUaeiouR~ML]/)
 	{
-		$syllables =~ s/[^0-9>](0+|1+|2+|3+|4+|5+|6+|7+|8+|9+)/<$1/g; # find onsets
-		$syllables =~ s/(0+|1+|2+|3+|4+|5+|6+|7+|8+|9+)[^0-9<]/$1>/g; # find codas
-		$syllables =~ s/<([0-9])/$1$1/g;
-		$syllables =~ s/([0-9])>/$1$1/g;
-	}
-	
-	# Find syllable break positions
-	$prevSyllable = substr($syllables,0,1);
-	for (my $i = 1; $i < length($syllables); $i++) 
-	{
-		$currSyllable = substr($syllables,$i,1);
-		if ($currSyllable != $prevSyllable)
+		$syllables =~ s/(9I)|(9U)|(OI)/--/g;  # diphthongs
+		$syllables =~ s/[&69AEIOUaeiouR~ML]/length($`)%10/eg; # vowels
+		$syllables =~ s/--/(length($`)%10) x 2/eg;  # diphthongs
+		# Loop until we've assigned all characters to a syllable
+		while ($syllables =~ m/[^0-9]/)
 		{
-			push @breaks, $i;
-			$prevSyllable = $currSyllable;
+			$syllables =~ s/[^0-9>](0+|1+|2+|3+|4+|5+|6+|7+|8+|9+)/<$1/g; # find onsets
+			$syllables =~ s/(0+|1+|2+|3+|4+|5+|6+|7+|8+|9+)[^0-9<]/$1>/g; # find codas
+			$syllables =~ s/<([0-9])/$1$1/g;
+			$syllables =~ s/([0-9])>/$1$1/g;
+		}
+	
+		# Find syllable break positions
+		$prevSyllable = substr($syllables,0,1);
+		for (my $i = 1; $i < length($syllables); $i++) 
+		{
+			$currSyllable = substr($syllables,$i,1);
+			if ($currSyllable != $prevSyllable)
+			{
+				push @breaks, $i;
+				$prevSyllable = $currSyllable;
 			
+			}
+		}
+	
+		# Insert syllable boundaries
+		for (my $i = 0; $i < scalar(@breaks); $i++) 
+		{
+			substr($line,$breaks[$i]+$i,0,".");
 		}
 	}
-	
-	# Insert syllable boundaries
-	for (my $i = 0; $i < scalar(@breaks); $i++) 
-	{
-		substr($line,$breaks[$i]+$i,0,".");
-	}
-	
 	print "$line\n";
 }
